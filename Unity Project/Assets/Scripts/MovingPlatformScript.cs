dc58659deb02df80e7208c56b5b590afe6fddde6
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 14:24:57 +0000

    MovingPlatform prefab added.

---
+++ Unity Project/Assets/Scripts/MovingPlatformScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class MovingPlatformScript : MonoBehaviour
+{
+	
+	[SerializeField]
+	private Vector3 moveTo;
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private bool loop;
+	private bool back;
+	private Vector3 a;
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		a = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (back) {
+			
+			if (transform.position != a) {
+				transform.position = Vector3.MoveTowards (transform.position, a, speed * Time.deltaTime);
+			} else {
+				back = false;
+			}
+			
+		} else {
+		
+			if (transform.position != moveTo) {
+				transform.position = Vector3.MoveTowards (transform.position, moveTo, speed * Time.deltaTime);
+			} else {
+				back = true;
+			}
+		}
+			
+	}
+}
