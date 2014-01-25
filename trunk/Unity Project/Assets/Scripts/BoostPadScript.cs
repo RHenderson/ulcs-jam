0abaf01b2014357ccec9564a1453795ae2031741
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 23:57:49 +0000

    BoostPad

---
+++ trunk/Unity Project/Assets/Scripts/BoostPadScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoostPadScript : MonoBehaviour {
+	
+	[SerializeField]
+	private Vector3 boost;
+	
+	void OnTriggerEnter(Collider car) {
+		Debug.Log(car.gameObject);
+		car.attachedRigidbody.AddForce(boost, ForceMode.VelocityChange);
+	}
+}
