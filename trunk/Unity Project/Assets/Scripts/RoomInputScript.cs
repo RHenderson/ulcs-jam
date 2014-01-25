1567be317c7d6d6f0c6f5ab509f86554de81a4cb
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 01:46:07 +0000

    Can open lid, added bow and walls.

---
+++ trunk/Unity Project/Assets/Scripts/RoomInputScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomInputScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetMouseButtonDown(0)) {
+	
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
+		
+		
+			if (Physics.Raycast(ray, out hit)) {
+				if (hit.transform.tag == "Lid") {
+					hit.collider.gameObject.GetComponent<ChestScript>().Open();
+					Debug.Log("Hit");
+				}
+			}
+			
+		}
+	}
+}
