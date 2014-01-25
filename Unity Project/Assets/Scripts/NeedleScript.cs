8890f92336456498fa8aa8e355aada194eafc568
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 15:25:39 +0000

    Added needle and needletrap prefabs.

---
+++ Unity Project/Assets/Scripts/NeedleScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleScript : MonoBehaviour {
+	
+	[SerializeField] private float speed = 10.0f;
+	[SerializeField] private int lifetime = 10;
+	
+	private Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, lifetime);
+	}
+	
+	void Update() {
+		
+		movement = new Vector3(speed, 0, 0);
+	}
+	
+	 void FixedUpdate()
+  {
+    // 5 - Move the game object
+    rigidbody.velocity = movement;
+  }
+}
---
+++ Unity Project/Assets/Scripts/NeedleTrapScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleTrapScript : MonoBehaviour
+{
+	
+	[SerializeField]
+	private Transform needle;
+	[SerializeField]
+	private float rateOfFire = 1;
+	[SerializeField]
+	private bool firing = true;
+	
+	void Start ()
+	{
+		StartCoroutine (Fire ());
+	}
+	
+	IEnumerator Fire ()
+	{
+		while (firing) {
+			Transform needleTrans = Instantiate (needle) as Transform;
+			needleTrans.position = transform.position;
+			yield return new WaitForSeconds(rateOfFire);
+		}
+	}
+}
