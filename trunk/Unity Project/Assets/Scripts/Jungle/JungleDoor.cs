9a1ffc7ac53070aab20ce9a29de82bd47068be7c
markpdolby@gmail.com <markpdolby@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 17:15:46 +0000

    +SkyBox

---
+++ trunk/Unity Project/Assets/Scripts/Jungle/JungleDoor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JungleDoor : InteractiveObject {
+    
+    [SerializeField] private Vector3 m_TargetRotation;
+    [SerializeField] private float m_OpenSpeed = 10;
+
+    public override void Interact()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Open());
+    }
+
+    private IEnumerator Open()
+    {
+        print("open");
+        while (Vector3.Distance(m_TargetRotation, transform.localEulerAngles) > 0.5f)
+        {
+            transform.localEulerAngles = Vector3.Slerp(transform.localEulerAngles, m_TargetRotation, Time.deltaTime * m_OpenSpeed);
+            yield return new WaitForEndOfFrame();
+        }
+        transform.localEulerAngles = m_TargetRotation;
+    }
+}
