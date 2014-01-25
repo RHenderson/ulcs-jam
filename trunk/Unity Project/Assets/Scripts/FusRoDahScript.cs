e7fbeba191bae1c41bd22c732e84472083b63a0b
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 16:38:55 +0000

    FUS DO RAH!

---
+++ trunk/Unity Project/Assets/Scripts/FusRoDahScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FusRoDahScript : MonoBehaviour
+{
+	[SerializeField]
+	private float force = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		if (Input.GetMouseButtonDown (0)) {
+	
+			Transform cam = Camera.main.transform;
+			RaycastHit hit = new RaycastHit ();
+		
+			if (Physics.Raycast (cam.position, cam.forward, out hit, 10)) {
+				if (hit.transform.tag == "Squirrel") {
+					if (hit.rigidbody == null) {
+						Rigidbody r = hit.collider.gameObject.AddComponent<Rigidbody> ();
+						r.mass = 0.1f;
+						r.AddExplosionForce (force, transform.position, 0);
+					}
+				}
+			}
+		}
+	}
+}
