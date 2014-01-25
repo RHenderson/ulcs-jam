6f5cb4fbf0ff46676714c677e469e2d0f16206cc
markpdolby@gmail.com <markpdolby@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 17:15:46 +0000

    +SkyBox

---
+++ Unity Project/Assets/Scripts/Jungle/TriggerPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPoint : MonoBehaviour {
+
+    [SerializeField] private InteractiveObject[] m_InteractiveObjects;
+
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (InteractiveObject io in m_InteractiveObjects)
+        {
+            io.Interact();
+        }
+    }
+}
