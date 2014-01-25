b19fc7713045b680101052d8928bb5e112a4e393
markpdolby@gmail.com <markpdolby@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 15:22:36 +0000

    +Mark Jungle scene

---
+++ Unity Project/Assets/Scripts/Jungle/JunglePlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JunglePlayer : MonoBehaviour {
+
+    [SerializeField] private GUIText m_GoldNumberText;
+
+    private int m_NumberOfGold;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Gold"))
+        {
+            DestroyObject(other.gameObject);
+            SetGold(1);
+            
+        }
+    }
+
+    private void SetGold(int amount)
+    {
+        m_NumberOfGold += amount;
+        m_GoldNumberText.text = m_NumberOfGold + "";
+    }
+
+}
