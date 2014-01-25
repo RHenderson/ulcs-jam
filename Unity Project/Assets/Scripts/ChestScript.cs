842cfc9119fbee95cbaa921f2597c720defb18f8
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 01:46:07 +0000

    Can open lid, added bow and walls.

---
+++ Unity Project/Assets/Scripts/ChestScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestScript : MonoBehaviour
+{
+	
+	private bool clickedLid;
+
+	// Use this for initialization
+	void Start ()
+	{
+		clickedLid = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	public void Open ()
+	{
+		if (!clickedLid) {
+			transform.Rotate (new Vector3 (90, 0, 0));
+			clickedLid = true;
+		}
+	}
+}
