d05875dc70ba0fe81c3c2b52012444b78cb93322
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-25 13:15:48 +0000

    Updated room.unity with some music resources and can now change to bow scene with fadeout and shrinking.

---
+++ Unity Project/Assets/Scripts/MementoScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MementoScript : MonoBehaviour {
+	
+	public string sceneToLoad;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void MoveLevel() {
+		Application.LoadLevel(1);
+	}
+}
