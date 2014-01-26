f8eb7899a151b7c57372bd584b789fb795b44638
rosshenderson92@gmail.com <rosshenderson92@gmail.com@0a78b955-f63a-9323-cada-87a8efed398b>
2014-01-26 01:10:38 +0000

    Added sound to RoomInputScript

--- trunk/Unity Project/Assets/Scripts/ChestScript.cs
+++ trunk/Unity Project/Assets/Scripts/ChestScript.cs
@@ -5,25 +5,30 @@ public class ChestScript : MonoBehaviour
 {
 	
 	private bool clickedLid;
-	private bool alreadyClicked;
-	private Vector3 rotVec;
 
 	// Use this for initialization
 	void Start ()
 	{
-		rotVec = new Vector3 (90, 0, 0);
+		clickedLid = false;
 	}
 	
-	public void Open() {
-		StartCoroutine(open());
-	}
-	
-	private IEnumerator open() {
-		while (transform.localEulerAngles.x < 350) {
-			Debug.Log("In");
-			transform.Rotate(rotVec * Time.deltaTime);
-			yield return new WaitForEndOfFrame();
-		}
+	// Update is called once per frame
+	void Update ()
+	{
 		
+		if (clickedLid)
+		{
+			//makes the lid spin
+			transform.Rotate(new Vector3 (90, 0, 0) * Time.deltaTime);
+			
+			if (transform.localEulerAngles.x > 350)
+				clickedLid = false;
+		}
+	
+	}
+	
+	public void Open ()
+	{
+		clickedLid = true;
 	}
 }
